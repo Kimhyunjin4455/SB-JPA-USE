4850a1b565a816bd0d2f0316c4d83b0d3c49b43e
Kimhyunjin4455 <dp4455@naver.com>
2023-08-01 01:17:18 +0900

    0801P065

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -3,10 +3,7 @@ package jpabook.jpashop.service;
 import jpabook.jpashop.Repository.ItemRepository;
 import jpabook.jpashop.Repository.MemberRepository;
 import jpabook.jpashop.Repository.OrderRepository;
-import jpabook.jpashop.domain.Delivery;
-import jpabook.jpashop.domain.DeliveryStatus;
-import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.*;
 import jpabook.jpashop.domain.item.Item;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -34,5 +31,25 @@ public class OrderService {
 
         //주문상품 생성
         OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        //주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        //주문 저장
+        orderRepository.save(order);
+        return order.getId();
     }
+
+    /**주문 취소*/
+    public void cancelOrder(Long orderId){
+        // 주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+        // 주문 조회
+        order.cancel();
+    }
+
+    /** 주문 검색 */ /*
+      public List<Order> findOrders(OrderSearch orderSearch) {
+          return orderRepository.findAll(orderSearch);
+} */
 }
